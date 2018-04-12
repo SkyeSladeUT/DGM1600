198dd54f87df1c91e01fe2dd35e8e545c7147f8d
Unknown <skyesladeut@outlook.com>
2018-04-12 16:24:18 -0600

    Used Scripts from other class

--- SideScroller/Assets/Scripts/Player.cs
+++ SideScroller/Assets/Scripts/Player.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class Player : ScriptableObject {
 
 	public int Score;
-	
+
 }
---
+++ SideScroller/Assets/Scripts/PowerUp/Trigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour {
+
+	public PowerUp PowerUpTransfer;
+	public Image HealthBar;
+	
+	//public Color FullHealth;
+	//public Color MidHealth;
+	//public Color LowHealth;
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		
+		if(HealthBar.fillAmount > 0) {
+			obj.GetComponent<MoveCharacter>().MovePattern = PowerUpTransfer.Transfer();
+			HealthBar.fillAmount -= PowerUpTransfer.PowerLevel;
+			/*if(HealthBar.fillAmount >= 0.70) {
+				HealthBar.color = FullHealth;
+			}
+			else if (HealthBar.fillAmount >= 0.30) {
+				HealthBar.color = MidHealth;
+			}
+			else if (HealthBar.fillAmount > 0.0) {
+				HealthBar.color = LowHealth;
+			}*/
+		}
+		gameObject.SetActive(!PowerUpTransfer.ObjectDestroy);
+	}
+
+}
