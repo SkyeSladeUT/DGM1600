5a8880df0f3527e7ee8300f4cbed489037fde48c
Unknown <skyesladeut@outlook.com>
2018-04-17 15:51:55 -0600

    Made Buttons to open and close doors

--- SideScroller/Assets/Scripts/Key.cs
+++ SideScroller/Assets/Scripts/Key.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class Key : MonoBehaviour {
 
-	public GameObject Door;
+	public GameObject[] Door1;
+	public GameObject[] Door2;
+	private bool CurrentButtonActive = true;
+	private Animation animate;
 
-	private void OnTriggerEnter(Collider other)
-	{
-		Destroy(Door);
-		
-		Destroy(this.gameObject);
+	private void Start () {
+		animate = gameObject.GetComponent<Animation> ();
+		foreach (var door in Door2) {
+			door.SetActive (false);
+		}
+		CurrentButtonActive = true;
+
 	}
+	private void OnTriggerExit (Collider other) {
+
+		if (CurrentButtonActive) {
+			animate.Play ("On");
+		} else {
+			animate.Play ("Off");
+		}
+		foreach (var door in Door1) {
+			door.SetActive (!CurrentButtonActive);
+		}
+		foreach (var door in Door2) {
+			door.SetActive (CurrentButtonActive);
+		}
+
+		CurrentButtonActive = !CurrentButtonActive;
+	}
+
 }
