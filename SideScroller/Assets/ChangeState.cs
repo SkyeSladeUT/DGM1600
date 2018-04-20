ef5df9c0a4c59cf74bed1bf2942dd4d0afa5f79d
Unknown <skyesladeut@outlook.com>
2018-04-20 15:37:46 -0600

    Deleted unnecessary code

--- SideScroller/Assets/ChangeState.cs
+++ SideScroller/Assets/ChangeState.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class ChangeState : StateMachineBehaviour {
 
 	public Player Player;
-	public MovePatter MovePattern;
+	public MovePattern MovePattern;
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Player.MovePattern = MovePattern;
+	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
